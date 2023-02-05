908e3ae157ac85eb31963077a72f39126ff0de96
straaaik <igortimst@gmail.com>
2023-02-05 16:35:49 +0300

    Интерфейс + рандомайзер вопросов

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+//Рандомайзер ответов
+//Дизайн сайта
+//Добавить создание собвственных вопросов
+//Добавить создание собвственных викторин
+
+
 const DATA = [
     {
         qustion: 'Столица России',
@@ -21,7 +27,7 @@ const DATA = [
         ]
     },
     {
-        qustion: 'Президент Росии',
+        qustion: 'Кто Президент Росcии',
         answers: [
             {
                 id: '4',
@@ -30,32 +36,38 @@ const DATA = [
             },
             {
                 id: '5',
-                value: 'Путин В.В',
-                correct: true
+                value: 'Я',
+                correct: false
             }
 
         ]
     },
     {
-        qustion: 'Президент Росии',
+        qustion: 'Ты еблан?',
         answers: [
             {
-                id: '4',
-                value: 'Путин В.В',
+                id: '6',
+                value: 'Да',
                 correct: true
             },
             {
-                id: '5',
-                value: 'Путин В.В',
-                correct: true
+                id: '7',
+                value: 'Нет',
+                correct: false
+            },
+            {
+                id: '8',
+                value: 'Сам еблан.',
+                correct: false
             }
 
         ]
-    }
+    },
+
 ];
 
-let ansResults = {
-};
+let ansResults = {};
+let arrNumber = [];
 
 const quiz = document.getElementById('quiz');
 const questions = document.getElementById('questions');
@@ -64,16 +76,34 @@ const indicator = document.getElementById('indicator');
 const btnNext = document.getElementById('btn-next');
 const btnRestart = document.getElementById('btn-restart');
 
+
 const renderQuestions = (index) => {
     renderIndicator(index + 1);
 
+    const maxQustions = DATA.length - 1;
+    const minQustions = 0;
+    const numReserve = () => {
+        const random = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        const randomNumber = random(minQustions, maxQustions);
+        if (arrNumber.includes(randomNumber)) {
+            numReserve();
+        }
+        else {
+            arrNumber.push(randomNumber);
+        }
+        return arrNumber[index];
+    }
+    const newIndex = numReserve();
+
     questions.dataset.currentStep = index;
 
-    const renderAnswers = () => DATA[index].answers
+    const renderAnswers = () => DATA[newIndex].answers
         .map((answer) => `
             <li>
                 <label>
-                    <input class="answer-input" type="radio" name=${index} value=${answer.id}>
+                    <input class="answer-input" type="radio" name=${newIndex} value=${answer.id}>
                     ${answer.value}
                 </label>
             </li>
@@ -83,7 +113,7 @@ const renderQuestions = (index) => {
 
     questions.innerHTML = `
         <div class="quiz-questions-item">
-    <div class="quiz-questions-item__question"> ${DATA[index].qustion}</div>
+    <div class="quiz-questions-item__question"> ${DATA[newIndex].qustion}</div>
     <ul class="quiz-questions-item__answers">${renderAnswers()}</ul>
     </div>
 `;
@@ -98,7 +128,7 @@ const renderResults = () => {
         if (!answer.correct && answer.id === ansResults[questionIndex]) {
             classname = 'answer--invalid';
         }
-        else if (answer.correct) {
+        else if (answer.correct && answer.id === ansResults[questionIndex]) {
             classname = 'answer--valid';
         }
 
@@ -165,6 +195,7 @@ quiz.addEventListener('click', (event) => {
     if (event.target.classList.contains('btn-restart')) {
         results.innerHTML = '';
         ansResults = {};
+        arrNumber = [];
 
         questions.classList.remove('questions--hidden');
         indicator.classList.remove('indicator--hidden');
@@ -174,6 +205,6 @@ quiz.addEventListener('click', (event) => {
 
         renderQuestions(0);
     }
-});
 
+});
 renderQuestions(0);
